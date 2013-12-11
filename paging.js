2941d8ca68d46e1be3d09bfa2232fe23d674bbd0
Boinx Bantug <boinxbantug25@yahoo.com>
2013-12-11 18:10:30 +0800

    almost done.

--- paging.js
+++ paging.js
@@ -4,9 +4,7 @@ $(document).ready(function(){
 
 		function switchPage(page){
 
-			// var x = 1;
-			// console.log("the value is"+ x + ".");
-			console.log(page);
+		console.log(page);
 		$("ul").removeClass("current");
 		$("ul:nth-child(" +page+ ")").addClass("current");
 
