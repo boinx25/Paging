965851ec1a002b11a36bfbe9a3ec1cb90c8efe09
Boinx Bantug <boinxbantug25@yahoo.com>
2013-12-13 10:30:29 +0800

    Done.

--- paging.js
+++ paging.js
@@ -1,22 +1,32 @@
 $(document).ready(function(){
 
-
+		var currentPage = 1;
+		var maxPage = ($("#navigator .number-paging").length);
 
 		function switchPage(page){
 
-		console.log(page);
+		currentPage = page;
+		
 		$("ul").removeClass("current");
 		$("ul:nth-child(" +page+ ")").addClass("current");
-
 		}		
+
 
 		$("#navigator a").click(function(){
 
+			if($(this).hasClass("nav2")){
+				if(currentPage < maxPage)
+				currentPage++;
+				switchPage(currentPage);
+			}
+			else if($(this).hasClass("nav")){
+				if(currentPage >=2)
+					currentPage--;
+				switchPage(currentPage);
+			}
+			else	
 			switchPage($(this).data("chosen"));
 
 
 		});
-
-
-	
 });
